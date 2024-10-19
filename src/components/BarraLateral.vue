1994fe79768bfc289c8a7c5516ce691e3e71c2be
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2024-10-18 22:56:17 -0300

    feature: form

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="logo" />
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BarraLateral",
+});
+</script>
+
+<style scoped>
+/* todo o estilo do componente */
+header {
+  background: #313331;
+  width: 100%;
+  height: 100vh;
+  padding: 2rem;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
